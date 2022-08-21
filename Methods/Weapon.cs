b9fdc4ce52dcded6764d1524c97580b3b9084723
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-08-21 19:57:37 +0400

    Initial commit

---
+++ Methods/Weapon.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PistolTask.Methods
+{
+    class Weapon
+    {
+        
+        private int _magazineCapacity;
+        private int _currentAmmoCount;
+        private double _magazineUnloadTime;
+        private string _fireMode;
+        public int MagazineCapacity 
+        {
+            get
+            {
+                return _magazineCapacity;
+            }
+            set
+            {
+                if (value>0)
+                {
+                    _magazineCapacity = value;
+                }
+            }
+        }
+        public int CurrentAmmoCount
+        {
+            get 
+            {
+                return _currentAmmoCount;
+            }
+            set 
+            {
+                if (value>=0 && value<=MagazineCapacity)
+                {
+                    _currentAmmoCount = value;
+                }
+            }
+        }
+        public double MagazineUnloadTime
+        {
+            get 
+            {
+                return _magazineUnloadTime;
+            }
+            set 
+            {
+                if (value>0)
+                {
+                    _magazineUnloadTime = value;
+
+                }
+            }
+        }
+        public string FireMode 
+        { get 
+            {
+                return _fireMode;
+            }
+            set 
+            {
+                if (value=="single" || value=="auto" || value=="Auto" || value=="Single")
+                {
+                    _fireMode = value;
+                }
+            } 
+        }
+
+        public Weapon(int magazineCapacity,int currentAmmoCount,double magazineEmptyTime,string fireMode)
+        {
+            MagazineCapacity = magazineCapacity;
+            CurrentAmmoCount =currentAmmoCount;
+            MagazineUnloadTime = magazineEmptyTime;
+            FireMode = fireMode;
+        }
+        public void Shoot()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (CurrentAmmoCount!=0)
+            {
+                CurrentAmmoCount = CurrentAmmoCount - 1;
+                Console.WriteLine("Shot");
+            }
+            else
+                Console.WriteLine("Magazine is empty!!!");
+        }
+        public void Fire() 
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (FireMode == "single" || FireMode == "Single")
+            {
+
+                if (CurrentAmmoCount != 0)
+                {
+                    CurrentAmmoCount = CurrentAmmoCount - 1;
+                    Console.WriteLine("Shot");
+                }
+                else
+                    Console.WriteLine("Magazine is empty!!!");
+
+            }
+            else 
+            {
+
+                double EachBulletSecond = MagazineUnloadTime / MagazineCapacity;
+                double needUnloadTime = CurrentAmmoCount * EachBulletSecond;
+                if (CurrentAmmoCount != 0)
+                {
+
+                    CurrentAmmoCount = CurrentAmmoCount - CurrentAmmoCount;
+                    Console.WriteLine("Shot");
+                    Console.WriteLine($"{needUnloadTime} seconds were spent to unload magazine");
+                }
+                else
+                    Console.WriteLine("Magazine is empty!!!");
+            }
+           
+        }
+        public int GetRemainBulletCount() 
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            
+            return MagazineCapacity - CurrentAmmoCount;
+
+        }
+        public void Reload() 
+        {
+            CurrentAmmoCount = CurrentAmmoCount + GetRemainBulletCount();
+            
+        }
+        public void ChangeFireMode() 
+        {
+            if (FireMode == "single" || FireMode=="Single")
+            {
+                FireMode = "auto";
+            }
+            else 
+            {
+                FireMode = "single";
+            }
+        }
+        public void ShowInfo() 
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (MagazineCapacity!=0 && FireMode!=null)
+            {
+                Console.WriteLine($"Magazine capacity:{MagazineCapacity}\n Current ammo:{CurrentAmmoCount}\n Total magazine unload time:{MagazineUnloadTime}\n Fire mode:{FireMode}");
+            }
+            else
+                Console.WriteLine("Wrong input!!!");
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using PistolTask.Methods;
+using System;
+
+namespace PistolTask
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+                Console.Write("Enter magazine capacity for gun:");
+                int magazineCapacity = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter current bullet count:");
+                int currentBullet = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter time(seconds) need to unload full magazine: ");
+                double timeUnload = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter fire mode: ");
+                string fireMode = Console.ReadLine();
+                Weapon gun = new Weapon(magazineCapacity, currentBullet, timeUnload, fireMode);
+                GetMenu(gun);
+    
+        }
+        static void Menu(Weapon gun, int check)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            switch (check)
+            {
+                case 0:
+                    gun.ShowInfo();
+                    break;
+                case 1:
+                    gun.Shoot();
+                    
+                    break;
+                case 2:
+                       gun.Fire();
+                    break;
+                case 3:
+                    Console.WriteLine(gun.GetRemainBulletCount()); 
+                    break;
+                case 4:
+                    gun.Reload();
+                    break;
+                case 5:
+                    gun.ChangeFireMode();
+                    break;
+                case 7:
+                    Console.WriteLine("Enter what do you want to do:\n 1.T (Change magazine capacity)\n 2.S (Change ammo count)\n 3.D (Change unload time)");
+                    char character = Convert.ToChar(Console.ReadLine());
+                    switch (character)
+                    {
+                        case 'T':
+                            Console.Write("Enter new size of magazine:");
+                            int newSize = Convert.ToInt32(Console.ReadLine());
+                            gun.MagazineCapacity = newSize;
+                            break;
+                        case 'S':
+                            Console.Write("Enter new amount of bullets:");
+                            int newBullets = Convert.ToInt32(Console.ReadLine());
+                            gun.CurrentAmmoCount = newBullets;
+                            break;
+                        case 'D':
+                            Console.Write("Enter new unload time:");
+                            double newUnload = Convert.ToDouble(Console.ReadLine());
+                            gun.MagazineUnloadTime = newUnload;
+                            break;
+                        default:
+                            Console.WriteLine("Wrong input!!!");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Wrong input!!!");
+                    break;
+            }
+            GetMenu(gun);
+        }
+        static void GetMenu(Weapon gun)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Weapon newGun = gun;
+
+            Console.WriteLine("Enter what do you want to do:\n 0.Information\n 1.Shoot one bullet\n 2.Fire all bullets\n 3.Get remain bullet count\n 4.Reload\n 5.Change fire mode\n 6.Exit from program\n 7.Change parametrs of gun");
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input != 6)
+            {
+                Menu(gun, input);
+            }
+            else 
+            {               
+                Console.WriteLine("Program finished");
+            }
+              
+        }
+ 
+
+    }
+}
